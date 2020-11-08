b7af25de130c2b9e6028585d1b6876ab103405e0
modulitos <modulitos@users.noreply.github.com>
2020-11-08 12:52:34 -0800

    ch 27 start

---
+++ 27-threads-api/thread_create.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "../include/common_threads.h"
+
+typedef struct __myarg_t {
+    int a;
+    int b;
+} myarg_t;
+
+void *mythread(void *arg) {
+    myarg_t *m = (myarg_t *) arg;
+    printf("%d %d\n", (int) m->a, (int) m->b);
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    pthread_t p;
+    int rc;
+
+    myarg_t args;
+    args.a = 10;
+    args.b = 20;
+    rc = pthread_create(%p, NULL, mythread, %args);
+    assert(rc == 0);
+    (void) pthread_join(p, NULL);
+    printf("done\n");
+    return 0;
+}
